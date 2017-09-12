e5f52855e52aa6648bc85566bf671ee1cbfe689e
Yuri2 <824831811@qq.com>
2017-09-12 10:31:48 +0800

    1.2.3

--- js/win10.js
+++ js/win10.js
@@ -2,7 +2,7 @@
  * Created by Yuri2 on 2017/7/10.
  */
 window.Win10 = {
-    _version:'v1.1.2.3(dev)',
+    _version:'v1.1.2.3',
     _debug:true,
     _bgs:{
         main:'',
@@ -230,6 +230,50 @@ window.Win10 = {
                 layero_opened.css('height',clientHeight-40);
                 layero_opened.find('.layui-layer-content').css('height',clientHeight-83);
                 layero_opened.find('.layui-layer-content iframe').css('height',clientHeight-83);
+            }
+        })
+    },
+
+    /**
+     * 原 win10_bind_open_windows 子窗口事件自动绑定插件
+     * @author:vG
+     * @修订:Yuri2
+     * @version:2.0.1
+     * 说明: 所有#win10下的元素加入类win10-open-window即可自动绑定openUrl函数，无须用onclick手动绑定
+     */
+    _bind_open_windows:function () {
+        // 注册事件委派 打开url窗口
+        $('#win10').on('click', '.win10-open-window', function() {
+            //>> 获取当前点击的对象
+            $this = $(this);
+            //>> 判断url地址是否为空 如果为空 不予处理
+            if($this.data('url') !== "") {
+                //>> 获取弹窗标题
+                var title = $this.data('title')||'',
+                    areaAndOffset;
+                //>> 判断是否有标题图片
+                var bg=$this.data('icon-bg')?$this.data('icon-bg'):'';
+                if($this.data('icon-image')) {
+                    //>> 加入到标题中
+                    title = '<img class="icon '+bg+'" src="' + $this.data('icon-image') + '"/>' + title;
+                }
+                if($this.data('icon-font')) {
+                    //>> 加入到标题中
+                    title = '<i class="fa fa-fw fa-'+$this.data('icon-font')+' icon '+bg+'"></i>' + title;
+                }
+                if(!title && $this.children('.icon').length===1 && $this.children('.title').length===1){
+                    title = $this.children('.icon').prop("outerHTML")+$this.children('.title').html();
+                }
+                //>> 判断是否需要 设置 区域宽度高度
+                if($this.data('area-offset')) {
+                    areaAndOffset = $this.data('area-offset');
+                    //>> 判断是否有分隔符
+                    if(areaAndOffset.indexOf(',')!==-1){
+                        areaAndOffset = eval(areaAndOffset);
+                    }
+                }
+                //>> 调用win10打开url方法
+                Win10.openUrl($this.data('url'), title, areaAndOffset);
             }
         })
     },
@@ -487,6 +531,8 @@ window.Win10 = {
             $("#win10 .desktop").append("<div id='win10-desktop-scene' style='width: 100%;height: calc(100% - 40px);position: absolute;left: 0;top: 0; z-index: 0;background-color: transparent;'></div>")
         }
 
+        //属性绑定
+        Win10._bind_open_windows();
     },
     setBgUrl:function (bgs) {
         this._bgs=bgs;
